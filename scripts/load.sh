9c2c0c28cb6d3db3b35af7d78c2081c520135b2a
yashodhah <yashodha.hettiarachchi@trustpayments.com>
2023-06-25 12:19:14 +0530

    minor changes

--- scripts/load.sh
+++ scripts/load.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-  hey -z 20s -c 500 -m POST -H "Content-Type: application/json" -d '{"content":"wow","owner":"unknown","done":"false"}' http://localhost:8080/api/todo
+  hey -z 10s -c 1000 -m POST -H "Content-Type: application/json" -d '{"content":"wow","owner":"unknown","done":"false"}' http://localhost:8080/api/todo
   json=$(curl http://localhost:8080/api/todo)
   echo $json
   content="A new content"
@@ -11,5 +11,5 @@ do
      echo '{ "id" : "'$id'", "content": "'$content'"}'
      curl --silent --output /dev/null -X PUT http://localhost:8080/api/todo -H "Content-Type: application/json" -d '{ "id" : "'"$id"'", "content": "'"$content"'"}'
     done
-  sleep 1;
+  sleep 0.5;
 done
